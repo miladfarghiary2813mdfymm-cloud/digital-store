2201008369e85af2b5611b959d357a1415fae2f5
milad <miladfarghiary2813.mdfy.mm@gmail.com>
2026-09-01 15:31:52 +0330

    Responsive-Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,71 +1,50 @@
-import { ShoppingCart, User, Search } from "lucide-react";
+import { ShoppingCart, User, Menu } from "lucide-react";
 function Navbar() {
   return (
     <nav className="mx-6 mt-6 rounded-2xl bg-white px-6 py-4 shadow-sm">
       <div className="flex items-center justify-between">
-
-    
         <div>
-          <h1 className="text-2xl font-bold">
-            دیجیتال شاپ
-          </h1>
+          <h1 className="text-2xl font-bold">دیجیتال شاپ</h1>
         </div>
 
-
-   
-        <ul className="flex items-center gap-8 text-lg font-medium">
-
+        <ul className="hidden items-center gap-8 text-lg font-medium md:flex">
           <li>
-            <a
-              href="#"
-              className="transition hover:text-blue-600"
-            >
+            <a href="#" className="transition hover:text-blue-600">
               خانه
             </a>
           </li>
 
           <li>
-            <a
-              href="#"
-              className="transition hover:text-blue-600"
-            >
+            <a href="#" className="transition hover:text-blue-600">
               محصولات
             </a>
           </li>
 
           <li>
-            <a
-              href="#"
-              className="transition hover:text-blue-600"
-            >
+            <a href="#" className="transition hover:text-blue-600">
               دسته‌بندی‌ها
             </a>
           </li>
-
         </ul>
 
-
-  
         <div className="flex items-center gap-3">
-
-       
           <input
             type="text"
             placeholder="جستجوی محصول..."
-            className="w-52 rounded-xl border border-gray-200 px-4 py-2.5 text-right outline-none transition focus:border-blue-500"
+            className=" hidden md:block w-52 rounded-xl border border-gray-200 px-4 py-2.5 text-right outline-none transition focus:border-blue-500"
           />
 
-          <button className="rounded-xl bg-blue-600 px-5 py-2.5 text-white transition hover:bg-blue-700">
-                <User />
+          <button className=" hidden md:flex rounded-xl bg-blue-600 px-5 py-2.5 text-white transition hover:bg-blue-700">
+            <User />
           </button>
-
 
           <button className="flex h-12 w-12 items-center justify-center rounded-xl transition hover:bg-gray-100">
-          <ShoppingCart />
+            <ShoppingCart  className="-scale-x-100"/>
           </button>
-
+          <button className="flex h-12 w-12 items-center justify-center rounded-xl transition hover:bg-gray-100 md:hidden">
+            <Menu />
+          </button>
         </div>
-
       </div>
     </nav>
   );
